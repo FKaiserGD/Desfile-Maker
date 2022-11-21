4fd399a002e35321ea8b831fb92177f39bfec39f
Filipe Lacerda <101232217+FKaiserGD@users.noreply.github.com>
2022-11-21 08:59:05 -0300

    Versão Oficial 1.0

---
+++ mysite/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+from flask.wrappers import Response
+import git
+
+app = Flask(__name__)
+
+@app.route('/galeria')
+def galeria():
+    return render_template("galeria.html")
+
+@app.route('/pecas')
+def pecas():
+    return render_template("pecas.html")
+
+@app.route('/cardin')
+def cardin():
+    return render_template("cardin.html")
+
+@app.route('/sobrenos')
+def sobre():
+    return render_template("sobrenos.html")
+
+@app.route('/voteaqui')
+def vote():
+    return render_template("voteaqui.html")
+
+@app.route('/')
+def principal():
+    return render_template("principal.html")
---
+++ mysite/static/menu.js
@@ -0,0 +1,17 @@
+let btnMenu = document.querySelector('.toggle-menu');
+let menuHamburguer = document.querySelector('.menu-hamburguer')
+let body = document.querySelector('body')
+let video = document.querySelector('#videos-link')
+let contatos = document.querySelector('.contatos-link')
+
+
+
+function menu(){
+  menuHamburguer.classList.toggle('show')
+  btnMenu.classList.toggle('active')
+  body.classList.toggle('block')
+}
+
+contatos.addEventListener("click", menu);
+btnMenu.addEventListener("click", menu);
+video.addEventListener("click", menu);
